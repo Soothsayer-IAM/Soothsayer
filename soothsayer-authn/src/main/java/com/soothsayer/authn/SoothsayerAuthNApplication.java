f6091d2d3156d3328c0f5afecd8344816076c139
Rory Gao <roryblucky@gmail.com>
2017-09-16 23:08:23 +0800

    initial commit
    Signed-off-by: Rory Gao <roryblucky@gmail.com>

---
+++ soothsayer-authn/src/main/java/com/soothsayer/authn/SoothsayerAuthNApplication.java
@@ -0,0 +1,12 @@
+package com.soothsayer.authn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoothsayerAuthNApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoothsayerAuthNApplication.class, args);
+    }
+}
